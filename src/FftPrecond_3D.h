f8209d6eff1b208f81a24c5ecd97eb0cdeec941d
KADOUCI Marwane 284423 <mk284423@is244526.intra.cea.fr>
2025-08-28 13:29:05 +0200

    Add header for the FFT 3D c file.

---
+++ src/FftPrecond_3D.h
@@ -0,0 +1,36 @@
+#ifndef FFT_PRECOND_3D_H
+#define FFT_PRECOND_3D_H
+
+#include <petscmat.h>
+#include <petscvec.h>
+
+struct CustomContext {
+    PetscInt n_x;
+    PetscInt n_y;
+    PetscInt n_z;
+    PetscScalar a_x;
+    PetscScalar a_y;
+    PetscScalar a_z;
+    PetscScalar dt;
+    PetscScalar delta_x;
+    PetscScalar delta_y;
+    PetscScalar delta_z;
+    Mat FFT_MAT;
+};
+
+PetscErrorCode Fft3DTransportSolver(PetscInt n_x, PetscInt n_y, PetscInt n_z,
+    PetscScalar a_x, PetscScalar a_y, PetscScalar a_z, PetscScalar dt,
+    PetscScalar delta_x, PetscScalar delta_y, PetscScalar delta_z,
+    Vec X, Vec b, Mat FFT_MAT);
+
+PetscErrorCode Fft2DTransportSolver(PetscInt n_x, PetscInt n_y,
+    PetscScalar a_x, PetscScalar a_y, PetscScalar dt,
+    PetscScalar delta_x, PetscScalar delta_y,
+    Vec X, Vec b, Mat FFT_MAT);
+
+PetscErrorCode Fft1DTransportSolver(PetscInt n_x, PetscScalar a_x, PetscScalar dt, PetscScalar delta_x, Vec X, Vec b, Mat FFT_MAT);
+
+PetscErrorCode PetscFft3DTransportSolver(struct CustomContext customCtx , Vec b, Vec x);
+
+
+#endif // FFT_PRECOND_3D_H
