ede2277c4cf316e45e1f4e83eaf8d57bed375b2f
KADOUCI Marwane 284423 <mk284423@is244526.intra.cea.fr>
2025-08-28 13:34:13 +0200

    Start wrapping and generalize 3D solver. Does not work yet.

--- src/FftPrecond_3D.c
+++ src/FftPrecond_3D.c
@@ -1,6 +1,8 @@
 #include <petscmat.h>
 #include <petscvec.h>
 
+#include "FftPrecond_3D.h"
+
 /* The function VecPointwiseDivideForRealFFT should be compiled only when petsc was buil with real numbers (not complex numbers) */
 #if !defined(PETSC_USE_COMPLEX)
 PetscErrorCode buildComplexRatio( PetscInt i, Vec v2, Vec v3, PetscReal* z1r, PetscReal* z1i) {
@@ -75,13 +77,227 @@ PetscErrorCode VecPointwiseDivideForRealFFT( Vec v1, Vec v2, Vec v3) {
 }
 #endif
 
-PetscErrorCode build_circulant_col_1D(Vec c, PetscInt size) {
+PetscErrorCode build_transport_col(Vec c, PetscInt size) {
     PetscFunctionBeginUser;
     VecSet(c, 0.0);
     VecSetValue(c, 0, 1.0, INSERT_VALUES);
     VecSetValue(c, 1, -1.0, INSERT_VALUES);
     PetscFunctionReturn(PETSC_SUCCESS);
 }
+
+PetscErrorCode vec_kronecker_product_identity_left(Vec c, Vec res, PetscInt c_size, PetscInt id_size, PetscScalar lambda) {
+    PetscFunctionBeginUser;
+    PetscScalar *c_array;
+    PetscScalar cur;
+
+    VecGetArray(c, &c_array);
+
+    for (PetscInt i = 0; i < c_size; i++) {
+        cur = c_array[i] * lambda;
+        for (PetscInt j = 0; j < id_size; j++) {
+            VecSetValue(res, j*c_size + i, cur, INSERT_VALUES);
+        }
+    }
+
+    VecRestoreArray(c, &c_array);
+
+    VecAssemblyBegin(c);
+    VecAssemblyEnd(c);
+
+    PetscFunctionReturn(PETSC_SUCCESS);
+}
+
+PetscErrorCode vec_kronecker_product_identity_right(Vec c, Vec res, PetscInt c_size, PetscInt id_size, PetscScalar lambda) {
+    PetscFunctionBeginUser;
+    PetscScalar *c_array;
+    PetscScalar cur;
+
+    VecGetArray(c, &c_array);
+
+    for (PetscInt i = 0; i < c_size; i++) {
+        cur = c_array[i] * lambda;
+        for (PetscInt j = 0; j < id_size; j++) {
+            VecSetValue(res, i*id_size + j, cur, INSERT_VALUES);
+        }
+    }
+
+    VecRestoreArray(c, &c_array);
+
+    VecAssemblyBegin(c);
+    VecAssemblyEnd(c);
+
+    PetscFunctionReturn(PETSC_SUCCESS);
+}
+
+PetscErrorCode build_diag_mat_vec_3D(Vec Diag, Vec c_x_hat, Vec c_y_hat, Vec c_z_hat, PetscInt n_x, PetscInt n_y, PetscInt n_z, PetscScalar lambda_x, PetscScalar lambda_y, PetscScalar lambda_z) {
+    PetscFunctionBeginUser;
+    Vec sum;
+
+    Vec kpi_x, kpi_y, kpi_z, kpi_y_intermediate;
+    VecDuplicate(Diag, &kpi_x);
+    VecDuplicate(Diag, &kpi_y);
+    VecDuplicate(Diag, &kpi_y_intermediate);
+    VecDuplicate(Diag, &kpi_z);
+
+    vec_kronecker_product_identity_left(c_x_hat, kpi_x, n_x, n_y * n_z, lambda_x);
+    vec_kronecker_product_identity_left(c_y_hat, kpi_y_intermediate, n_y, n_z, lambda_y);
+    vec_kronecker_product_identity_right(kpi_y_intermediate, kpi_y, n_y * n_z, n_x, 1);
+    vec_kronecker_product_identity_right(c_z_hat, kpi_z, n_z, n_x * n_y, lambda_z);
+
+    PetscCall(VecDuplicate(kpi_x, &sum));
+    PetscCall(VecAXPY(sum, 1.0, kpi_x));
+    PetscCall(VecAXPY(sum, 1.0, kpi_y));
+    PetscCall(VecAXPY(sum, 1.0, kpi_z));
+    PetscCall(VecShift(sum, 1.0));
+
+    PetscCall(VecCopy(sum, Diag));
+
+    PetscCall(VecDestroy(&kpi_x));
+    PetscCall(VecDestroy(&kpi_y));
+    PetscCall(VecDestroy(&kpi_z));
+    PetscCall(VecDestroy(&sum));
+    PetscFunctionReturn(PETSC_SUCCESS);
+}
+
+PetscErrorCode solve_3D(Mat FFT_MAT, Vec X, Vec Diag, Vec b, Vec b_hat) {
+    PetscFunctionBeginUser;
+    PetscInt size;
+
+    // FFT
+    PetscCall(MatMult(FFT_MAT, b, b_hat));
+
+    // b_hat / Diag
+#if defined(PETSC_USE_COMPLEX)
+    PetscCall(VecPointwiseDivide(b_hat, b_hat, Diag));
+#else
+    PetscCall(VecPointwiseDivideForRealFFT(b_hat, b_hat, Diag));
+#endif
+
+    // IFFT
+    PetscCall(MatMultTranspose(FFT_MAT, b_hat, X));
+
+    // Normalize
+    PetscCall(VecGetSize(X, &size));
+#if defined(PETSC_USE_COMPLEX)
+    VecScale(X,1./size);
+#else
+    VecScale(X,2./size);
+#endif
+
+    PetscFunctionReturn(PETSC_SUCCESS);
+}
+
+PetscErrorCode Fft3DSolver(PetscInt n_x, PetscInt n_y, PetscInt n_z,
+    PetscScalar a_x, PetscScalar a_y, PetscScalar a_z, PetscScalar dt,
+    PetscScalar delta_x, PetscScalar delta_y, PetscScalar delta_z,
+    Vec X, Vec b, Mat FFT_MAT, Vec c_x, Vec c_y, Vec c_z) {
+    
+    PetscFunctionBeginUser;
+
+    // Variables
+    Vec b_hat; // fft of b
+    Vec Diag; // vector of the diagonal matrix
+
+    // Input parameters
+    PetscScalar lambda_x = a_x * dt / delta_x;
+    PetscScalar lambda_y = a_y * dt / delta_y;
+    PetscScalar lambda_z = a_z * dt / delta_z;
+    
+    // Initialize vectors
+    PetscCall(MatCreateVecsFFTW( FFT_MAT, NULL, &Diag, NULL));
+    PetscCall(MatCreateVecsFFTW( FFT_MAT, NULL, &b_hat, NULL));
+
+    // Solve the system
+    build_diag_mat_vec_3D(Diag, c_x, c_y, c_z, n_x, n_y, n_z, lambda_x, lambda_y, lambda_z);
+    solve_3D(FFT_MAT, X, Diag, b, b_hat);
+
+    // Clean up
+    PetscCall(VecDestroy(&Diag));
+    PetscCall(VecDestroy(&b_hat));
+    PetscCall(MatDestroy(&FFT_MAT));
+
+    PetscFunctionReturn(PETSC_SUCCESS);
+}
+
+PetscErrorCode Fft3DTransportSolver(PetscInt n_x, PetscInt n_y, PetscInt n_z,
+    PetscScalar a_x, PetscScalar a_y, PetscScalar a_z, PetscScalar dt,
+    PetscScalar delta_x, PetscScalar delta_y, PetscScalar delta_z,
+    Vec X, Vec b, Mat FFT_MAT) {
+    
+    PetscFunctionBeginUser;
+
+    Vec c_x, c_y, c_z;
+    Vec c_x_hat, c_y_hat, c_z_hat;
+    Mat FFT_cx, FFT_cy, FFT_cz;
+    
+    PetscInt ndim_x = 1;
+    PetscInt ndim_y = 1;
+    PetscInt ndim_z = 1;
+    PetscInt dims_x[1] = {n_x};
+    PetscInt dims_y[1] = {n_y};
+    PetscInt dims_z[1] = {n_z};
+
+    PetscCall(MatCreateFFT(PETSC_COMM_WORLD, ndim_x, dims_x, MATFFTW, &FFT_cx));
+    PetscCall(MatCreateFFT(PETSC_COMM_WORLD, ndim_y, dims_y, MATFFTW, &FFT_cy));
+    PetscCall(MatCreateFFT(PETSC_COMM_WORLD, ndim_z, dims_z, MATFFTW, &FFT_cz));
+
+    PetscCall(MatCreateVecsFFTW(FFT_cx, &c_x, &c_x_hat, NULL));
+    PetscCall(MatCreateVecsFFTW(FFT_cy, &c_y, &c_y_hat, NULL));
+    PetscCall(MatCreateVecsFFTW(FFT_cz, &c_z, &c_z_hat, NULL));
+
+    build_transport_col(c_x, n_x);
+    build_transport_col(c_y, n_y);
+    build_transport_col(c_z, n_z);
+
+    MatMult(FFT_cx, c_x, c_x_hat);
+    MatMult(FFT_cy, c_y, c_y_hat);
+    MatMult(FFT_cz, c_z, c_z_hat);
+    
+    PetscCall(Fft3DSolver(n_x, n_y, n_z, a_x, a_y, a_z, dt, delta_x, delta_y, delta_z, X, b, FFT_MAT, c_x, c_y, c_z));
+    
+    PetscCall(VecDestroy(&c_x));
+    PetscCall(VecDestroy(&c_y));
+    PetscCall(VecDestroy(&c_z));
+
+    PetscFunctionReturn(PETSC_SUCCESS);
+}
+
+PetscErrorCode Fft2DTransportSolver(PetscInt n_x, PetscInt n_y,
+    PetscScalar a_x, PetscScalar a_y, PetscScalar dt,
+    PetscScalar delta_x, PetscScalar delta_y,
+    Vec X, Vec b, Mat FFT_MAT) {
+    
+    PetscFunctionBeginUser;
+
+    PetscCall(Fft3DTransportSolver(n_x, n_y, 1, a_x, a_y, 0, dt, delta_x, delta_y, 1, X, b, FFT_MAT));
+    
+    PetscFunctionReturn(PETSC_SUCCESS);
+}
+
+PetscErrorCode Fft1DTransportSolver(PetscInt n_x, PetscScalar a_x, PetscScalar dt, PetscScalar delta_x, Vec X, Vec b, Mat FFT_MAT) {
+    PetscFunctionBeginUser;
+
+    PetscCall(Fft3DTransportSolver(n_x, 1, 1, a_x, 0, 0, dt, delta_x, 1, 1, X, b, FFT_MAT));
+
+    PetscFunctionReturn(PETSC_SUCCESS);
+}
+
+PetscErrorCode PetscFft3DTransportSolver(struct CustomContext customCtx , Vec b, Vec x) {
+    PetscFunctionBeginUser;
+    
+    PetscCall(Fft3DTransportSolver(customCtx.n_x, customCtx.n_y, customCtx.n_z,
+                         customCtx.a_x, customCtx.a_y, customCtx.a_z, customCtx.dt,
+                         customCtx.delta_x, customCtx.delta_y, customCtx.delta_z,
+                         x, b, customCtx.FFT_MAT));
+
+    PetscFunctionReturn(PETSC_SUCCESS);
+}
+
+
+
+/* ######### TESTS ############ */
+
+#include "FftPrecond_3D.h"
 
 PetscErrorCode build_circulant_mat_1D( Mat C, PetscInt size, PetscScalar c1, PetscScalar c2) {
     PetscFunctionBeginUser;
@@ -171,140 +387,6 @@ PetscErrorCode build_C_3D(Mat *C, PetscInt n_x, PetscInt n_y, PetscInt n_z, Pets
     PetscFunctionReturn(PETSC_SUCCESS);
 }
 
-PetscErrorCode vec_kronecker_product_identity_left(Vec c, Vec res, PetscInt c_size, PetscInt id_size, PetscScalar lambda) {
-    PetscFunctionBeginUser;
-    PetscScalar *c_array;
-    PetscScalar cur;
-
-    VecGetArray(c, &c_array);
-
-    for (PetscInt i = 0; i < c_size; i++) {
-        cur = c_array[i] * lambda;
-        for (PetscInt j = 0; j < id_size; j++) {
-            VecSetValue(res, j*c_size + i, cur, INSERT_VALUES);
-        }
-    }
-
-    VecRestoreArray(c, &c_array);
-
-    VecAssemblyBegin(c);
-    VecAssemblyEnd(c);
-
-    PetscFunctionReturn(PETSC_SUCCESS);
-}
-
-PetscErrorCode vec_kronecker_product_identity_right(Vec c, Vec res, PetscInt c_size, PetscInt id_size, PetscScalar lambda) {
-    PetscFunctionBeginUser;
-    PetscScalar *c_array;
-    PetscScalar cur;
-
-    VecGetArray(c, &c_array);
-
-    for (PetscInt i = 0; i < c_size; i++) {
-        cur = c_array[i] * lambda;
-        for (PetscInt j = 0; j < id_size; j++) {
-            VecSetValue(res, i*id_size + j, cur, INSERT_VALUES);
-        }
-    }
-
-    VecRestoreArray(c, &c_array);
-
-    VecAssemblyBegin(c);
-    VecAssemblyEnd(c);
-
-    PetscFunctionReturn(PETSC_SUCCESS);
-}
-
-PetscErrorCode build_diag_mat_vec_3D(Vec c, PetscInt n_x, PetscInt n_y, PetscInt n_z, PetscScalar lambda_x, PetscScalar lambda_y, PetscScalar lambda_z) {
-    PetscFunctionBeginUser;
-    Mat FFT_cx, FFT_cy, FFT_cz;
-    Vec c_x, c_y, c_z, c_x_hat, c_y_hat, c_z_hat, sum;
-
-    // Initialize FFT 1D
-    PetscInt ndim_x = 1;
-    PetscInt ndim_y = 1;
-    PetscInt ndim_z = 1;
-    PetscInt dims_x[1] = {n_x};
-    PetscInt dims_y[1] = {n_y};
-    PetscInt dims_z[1] = {n_z};
-
-    PetscCall(MatCreateFFT(PETSC_COMM_WORLD, ndim_x, dims_x, MATFFTW, &FFT_cx));
-    PetscCall(MatCreateFFT(PETSC_COMM_WORLD, ndim_y, dims_y, MATFFTW, &FFT_cy));
-    PetscCall(MatCreateFFT(PETSC_COMM_WORLD, ndim_z, dims_z, MATFFTW, &FFT_cz));
-
-    PetscCall(MatCreateVecsFFTW(FFT_cx, &c_x, &c_x_hat, NULL));
-    PetscCall(MatCreateVecsFFTW(FFT_cy, &c_y, &c_y_hat, NULL));
-    PetscCall(MatCreateVecsFFTW(FFT_cz, &c_z, &c_z_hat, NULL));
-
-    build_circulant_col_1D(c_x, n_x);
-    build_circulant_col_1D(c_y, n_y);
-    build_circulant_col_1D(c_z, n_z);
-
-    MatMult(FFT_cx, c_x, c_x_hat);
-    MatMult(FFT_cy, c_y, c_y_hat);
-    MatMult(FFT_cz, c_z, c_z_hat);
-
-    Vec kpi_x, kpi_y, kpi_z, kpi_y_intermediate;
-    VecDuplicate(c, &kpi_x);
-    VecDuplicate(c, &kpi_y);
-    VecDuplicate(c, &kpi_y_intermediate);
-    VecDuplicate(c, &kpi_z);
-
-    vec_kronecker_product_identity_left(c_x_hat, kpi_x, n_x, n_y * n_z, lambda_x);
-    vec_kronecker_product_identity_left(c_y_hat, kpi_y_intermediate, n_y, n_z, lambda_y);
-    vec_kronecker_product_identity_right(kpi_y_intermediate, kpi_y, n_y * n_z, n_x, 1);
-    vec_kronecker_product_identity_right(c_z_hat, kpi_z, n_z, n_x * n_y, lambda_z);
-
-    PetscCall(VecDuplicate(kpi_x, &sum));
-    PetscCall(VecAXPY(sum, 1.0, kpi_x));
-    PetscCall(VecAXPY(sum, 1.0, kpi_y));
-    PetscCall(VecAXPY(sum, 1.0, kpi_z));
-    PetscCall(VecShift(sum, 1.0));
-
-    PetscCall(VecCopy(sum, c));
-
-
-    PetscCall(VecDestroy(&kpi_x));
-    PetscCall(VecDestroy(&kpi_y));
-    PetscCall(VecDestroy(&kpi_z));
-    PetscCall(VecDestroy(&c_x));
-    PetscCall(VecDestroy(&c_y));
-    PetscCall(VecDestroy(&c_z));
-    PetscCall(VecDestroy(&sum));
-    PetscCall(MatDestroy(&FFT_cx));
-    PetscCall(MatDestroy(&FFT_cy));
-    PetscCall(MatDestroy(&FFT_cz));
-    PetscFunctionReturn(PETSC_SUCCESS);
-}
-
-PetscErrorCode solve_3D(Mat FFT_MAT, Vec X, Vec Diag, Vec b, Vec b_hat) {
-    PetscFunctionBeginUser;
-    PetscInt size;
-
-    // FFT
-    PetscCall(MatMult(FFT_MAT, b, b_hat));
-
-    // b_hat / Diag
-#if defined(PETSC_USE_COMPLEX)
-    PetscCall(VecPointwiseDivide(b_hat, b_hat, Diag));
-#else
-    PetscCall(VecPointwiseDivideForRealFFT(b_hat, b_hat, Diag));
-#endif
-
-    // IFFT
-    PetscCall(MatMultTranspose(FFT_MAT, b_hat, X));
-
-    // Normalize
-    PetscCall(VecGetSize(X, &size));
-#if defined(PETSC_USE_COMPLEX)
-    VecScale(X,1./size);
-#else
-    VecScale(X,2./size);
-#endif
-
-    PetscFunctionReturn(PETSC_SUCCESS);
-}
-
 PetscErrorCode test_3D() {
     PetscFunctionBeginUser;
 
@@ -319,6 +401,7 @@ PetscErrorCode test_3D() {
     PetscScalar delta_x = 1;
     PetscScalar delta_y = 1;
     PetscScalar delta_z = 1;
+
     PetscScalar lambda_x = a_x * dt / delta_x;
     PetscScalar lambda_y = a_y * dt / delta_y;
     PetscScalar lambda_z = a_z * dt / delta_z;
@@ -327,21 +410,19 @@ PetscErrorCode test_3D() {
     Mat C; // Block-circulant matrix
     Vec X_ref, X, b; // CX=b
     Mat FFT_MAT;
-    Vec b_hat; // fft of b
-    Vec Diag; // vector of the diagonal matrix
 
     Vec r;// residual vector r=AX-b
     PetscReal rnorm, enorm;// residual norm and error
     PetscReal X_ref_norm, b_norm;
 
-    // Initialize FFT 2D
+    // Initialize FFT 3D
     PetscInt ndim = 3;
     PetscInt dims[3] = {n_z, n_y, n_x};
     PetscCall(MatCreateFFT(PETSC_COMM_WORLD, ndim, dims, MATFFTW, &FFT_MAT));
     
     // Initialize vectors
-    PetscCall(MatCreateVecsFFTW( FFT_MAT, NULL, &Diag, &X_ref));
-    PetscCall(MatCreateVecsFFTW( FFT_MAT, &b, &b_hat  , &X));
+    PetscCall(MatCreateVecsFFTW( FFT_MAT, NULL, NULL, &X_ref));
+    PetscCall(MatCreateVecsFFTW( FFT_MAT, &b, NULL, &X));
 
     // Build the block-circulant matrix C
     PetscCall(MatCreateAIJ(PETSC_COMM_WORLD, n_x * n_y * n_z, n_x * n_y * n_z, n_x * n_y * n_z, n_x * n_y * n_z, 2, NULL, 1, NULL, &C));
@@ -356,8 +437,7 @@ PetscErrorCode test_3D() {
     PetscCall(MatMult(C, X_ref, b));
 
     // Solve the system
-    build_diag_mat_vec_3D(Diag, n_x, n_y, n_z, lambda_x, lambda_y, lambda_z);
-    solve_3D(FFT_MAT, X, Diag, b, b_hat);
+    Fft3DTransportSolver(n_x, n_y, n_z, a_x, a_y, a_z, dt, delta_x, delta_y, delta_z, X, b, FFT_MAT);
 
     // Compute relative Residual
     VecNorm(b, NORM_2, &b_norm);
@@ -395,10 +475,7 @@ PetscErrorCode test_3D() {
     PetscCall(VecDestroy(&b));
     PetscCall(VecDestroy(&X));
     PetscCall(VecDestroy(&X_ref));
-    PetscCall(VecDestroy(&b_hat));
-    PetscCall(VecDestroy(&Diag));
     PetscCall(VecDestroy(&r));
-    PetscCall(MatDestroy(&FFT_MAT));
     PetscCall(MatDestroy(&C));
 
     PetscFunctionReturn(PETSC_SUCCESS);
